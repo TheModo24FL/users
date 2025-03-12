22a462c0f0a1c0c61b84e6899adecc1ba2ba27c7
teodoraturculet <teodora_turculet@yahoo.ro>
2025-03-12 16:43:31 +0200

    Inserare utilizatori in DB + Criptare parola

--- proiect-gestionare-utilizatori/assets/js/script.js
+++ proiect-gestionare-utilizatori/assets/js/script.js
@@ -1,4 +1,31 @@
 // Add your JavaScript code here
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('JavaScript is loaded');
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     console.log('JavaScript is loaded');
+// });
+
+const bcrypt = require('bcrypt')
+
+const hashPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(10, (err, salt) => {
+            if(err) {
+                reject(err)
+            }
+            bcrypt.hash(password, salt, (err, hash) => {
+                if (err){
+                    reject(err)
+                }
+                resolve(hash)
+            })
+        })
+    })
+}
+
+const comparePasswords = (password, hashedPassword) => {
+    return bcrypt.compare(password, hashedPassword)
+}
+
+module.exports = {
+    hashPassword,
+    comparePasswords
+}
